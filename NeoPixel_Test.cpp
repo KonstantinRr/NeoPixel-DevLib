7dd22e92a88a69f4bc12e384763d6808f182a494
Konstantin Rolf <konstantin.rolf@gmail.com>
2020-07-26 00:32:34 +0200

    Added test + examples

--- NeoPixel_Test.cpp
+++ NeoPixel_Test.cpp
@@ -20,27 +20,41 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 /// SOFTWARE.
 
+#ifdef NEOPIXEL_TEST
+
+#ifndef DONT_INCLUDE_ADA
+#include <Adafruit_NeoPixel.h>
+#endif
 #include "NeoPixel_Wrapper.h"
 
-/*
-Adafruit_NeoPixel strips[] = {
-    Adafruit_NeoPixel(60, 3, NEO_GRB + NEO_KHZ800),
-    Adafruit_NeoPixel(57, 6, NEO_GRB + NEO_KHZ800),
-    Adafruit_NeoPixel(57, 10, NEO_GRB + NEO_KHZ800),
-    Adafruit_NeoPixel(60, 11, NEO_GRB + NEO_KHZ800)
-};
 
+
+// Declare our NeoPixel strip object:
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+NeopixelWrapper strips[] = {
+    NeopixelWrapper(60, 11, NEO_GRB + NEO_KHZ800),
+    NeopixelWrapper(60, 3, NEO_GRB + NEO_KHZ800, true),
+    NeopixelWrapper(60, 6, NEO_GRB + NEO_KHZ800),
+    NeopixelWrapper(60, 10, NEO_GRB + NEO_KHZ800, true),
+};
+// Combines the strips to a single strip
+MultilineWrapper strip(strips, 4);
 
 void setup() {
-  for (auto& ref : strips) {
-    ref.begin();
-    ref.setBrightness(50);
-    ref.show();
-  }
+  strip.begin();
 }
-
 
 void loop() {
-
+    strip.clear();
+    strip.fill(Adafruit_NeoPixel::Color(255, 255, 255));
+    strip.show();
 }
-*/
+
+#endif
